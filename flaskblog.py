1e196131a627f9e57b263be1847a7e078a5ab606
Rorywork <rorywork95@outlook.com>
2019-12-16 16:27:48 +0000

    Added Bootstrap boielrplate to new layout file and linked it to other pages

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template
 # Use the below command as a workaround to get the terminal to run the flask site
 # set FLASK_DEBUG=1 && python -m flask run
 # Below can be used if you are running the flaskblog module with python locally
@@ -7,17 +7,33 @@ from flask import Flask, escape, request
 # Tells Flask where to look for templates and static files
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Rory Lindsay',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2019'
+    },
+    {
+        'author': 'Ciara Lindsay',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 23, 2019'
+    }
+]       
+
+
 
 # Forward slash is just the root/home page of the website
 @app.route('/')
 @app.route('/home')
 def home():
-    return "<h1>Hello Rory</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 
 
