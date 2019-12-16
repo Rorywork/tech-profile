a6d38b868cb257da948eeb3d7a2740a35e930483
Rorywork <rorywork95@outlook.com>
2019-12-16 16:01:52 +0000

    Added about route to flaskblog.py

--- flaskblog.py
+++ flaskblog.py
@@ -1,12 +1,23 @@
 from flask import Flask, escape, request
-
+# Use the below command as a workaround to get the terminal to run the flask site
+# set FLASK_DEBUG=1 && python -m flask run
+# Below can be used if you are running the flaskblog module with python locally
+# if __name__ == '__main__':
+#     app.run(debug=True)
 # Tells Flask where to look for templates and static files
 app = Flask(__name__)
 
+
 # Forward slash is just the root/home page of the website
 @app.route('/')
-def hello():
-    return "<h1>Hello Home</h1>"
+@app.route('/home')
+def home():
+    return "<h1>Hello Rory</h1>"
 
-# Use the below command as a workaround to get the terminal to run the flask site
-# set FLASK_DEBUG=1 && python -m flask run
+
+@app.route('/about')
+def about():
+    return "<h1>About Page</h1>"
+
+
+
